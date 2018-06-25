c340aa8d26281574cae88f2462c2455b7da3e8f2
Christopher Beards <beardscm@gmail.com>
2018-06-25 10:52:00 -0400

    App Starts; Initial Design Established

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{height: '300px', position: 'relative'}}>
+          <Layout fixedHeader>
+              <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+                  <Navigation>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                      <a href="#">Link</a>
+                  </Navigation>
+              </Drawer>
+              <Content />
+          </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
